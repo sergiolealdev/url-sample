b4b5bbb9322e33fb883f820bd7d9cf90606dc4d3
Sergio Leal <sergio@sergiolealdev.com>
2017-02-21 18:48:04 +0000

    changing main

--- src/com/sergiolealdev/UrlMain.java
+++ src/com/sergiolealdev/UrlMain.java
@@ -9,15 +9,31 @@ import java.net.URL;
 public class UrlMain {
 
 	public static void main(String[] args) throws URISyntaxException, IOException {
-		URI uri = new URI("http://java.sun.com/");
+		URI uri = new URI("http://as.com");
 		 URL url = uri.toURL();
 		 InputStream in = url.openStream();
 		 int i;
 		 char c;
+		 StringBuffer sb = new StringBuffer();
 		 while((i=in.read())!=-1){
 			 c=(char)i;
 			 System.out.print(c);
+			 sb.append(c);
 		 }
+		 
+		 String result = sb.toString();
+		 String cristianoString = "Cristiano";
+		 String messiString = "Messi";
+		 int cristianoOccurences = 0;
+		 int messiOccurences = 0;
+		  for (int index = result.indexOf(cristianoString);index >= 0; index = result.indexOf(cristianoString, index + 1)) {
+			  cristianoOccurences++;
+		    }
+		  for (int index = result.indexOf(messiString);index >= 0; index = result.indexOf(messiString, index + 1)) {
+			  messiOccurences++;
+		    }
+		  System.out.println("Found " + cristianoOccurences + " Cristiano Occurrences");
+		 System.out.println("Found " + messiOccurences + " Messi Occurrences");
 
 	}
 
